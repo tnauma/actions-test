dbde6c447f64c72af4a610f779cfb9383df93cd8
Torben Naumann <8687434+tnaumann91@users.noreply.github.com>
2023-02-12 14:29:30 +0100

    PHP CS Fixer installiert

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('somedir')
+    ->notPath('src/Symfony/Component/Translation/Tests/fixtures/resources.php')
+    ->in(__DIR__)
+;
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+    '@PSR12' => true,
+    'strict_param' => true,
+    'array_syntax' => ['syntax' => 'short'],
+])
+    ->setFinder($finder)
+    ;
